86e471eee032a02a5b4328b4194d1a862b6d6c1e
Mark Wilkinson <markawil@yahoo.com>
2017-11-05 12:13:55 -0600

    using navBar fade animation instead

--- LoremPixelAnimatingViews/ViewController.swift
+++ LoremPixelAnimatingViews/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     var lastSelectedIndexPath: IndexPath?
     var isAnimatingAway: Bool = false
     var isAnimatingBack: Bool = false
+    var isFirstLaunchScroll = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,12 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
         let size = (self.view.bounds.width/2.0) - 5
         layout.itemSize = CGSize(width: size, height: size)
+        
+        isFirstLaunchScroll = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -47,6 +54,12 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         super.viewWillAppear(animated)
         
         navigationController?.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        isFirstLaunchScroll = false
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -82,18 +95,22 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             if(scrollView.panGestureRecognizer.translation(in: scrollView.superview).y > 0)
             {
                 // scrolling upward, (or scrolling down to see more cells)
-                if isAnimatingBack == false && validNavController.navigationBar.isHidden == true {
+                if isAnimatingBack == false && validNavController.navigationBar.alpha < 1.0 {
                     isAnimatingBack = true
-                    validNavController.setNavigationBarHidden(false, animated: true)
+                    UIView.animate(withDuration: 0.5, animations: {
+                        validNavController.navigationBar.alpha = 1.0
+                    })
                 } else {
                     isAnimatingBack = false
                 }
             }
-            else {
+            else if !isFirstLaunchScroll {
                 // scrolling downward (or scrolling back up to the top cells)
-                if isAnimatingAway == false && validNavController.navigationBar.isHidden == false {
+                if isAnimatingAway == false && validNavController.navigationBar.alpha > 0.0 {
                     isAnimatingAway = true
-                    validNavController.setNavigationBarHidden(true, animated: true)
+                    UIView.animate(withDuration: 0.5, animations: {
+                        validNavController.navigationBar.alpha = 0.0
+                    })
                 } else {
                     // it's done hiding
                     isAnimatingAway = false
